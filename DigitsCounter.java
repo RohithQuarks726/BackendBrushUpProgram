1b2a9a2699b66ed6bc639dd4fa676d2e038630d1
RohithQuarks726 <154428777+RohithQuarks726@users.noreply.github.com>
2024-01-03 10:39:59 +0530

    Add files via upload

---
+++ DigitsCounter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class DigitsCounter {
+    public static void main(String[] args){
+        DigitCounter digObj=new DigitCounter();
+        digObj.digitCounter();
+    }
+}
+class DigitCounter{
+    Scanner scanObj=new Scanner(System.in);
+    public void digitCounter(){
+        long num=scanObj.nextLong();
+        long a=num;
+        int c=0;
+        while (a!=0){
+            a/=10;
+            c++;
+        }
+        System.out.println("No of digits in "+num+" is "+c);
+    }
+}
